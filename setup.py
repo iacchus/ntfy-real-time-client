4f53c19a7ea5ca4d18f5fe27206698e518445979
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2025-06-04 11:29:04 -0300

    commit

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-    Setup file for python-pushover-open-client.
+    Setup file for ntfy-real-time-client.
     Use setup.cfg to configure your project.
 
     This file was generated with PyScaffold 4.1.4.
